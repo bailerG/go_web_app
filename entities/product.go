1968a65a68f254fbf7f48997cbf0724bc6501444
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2024-05-23 21:03:42 -0300

    Created server and an index template

---
+++ entities/product.go
@@ -0,0 +1,8 @@
+package entities
+
+type Product struct {
+	Name     string
+	Desc     string
+	Price    float64
+	Quantity int
+}
--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"go_web_app/entities"
+	"net/http"
+	"text/template"
+)
 
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		return
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	products := []entities.Product{
+		{"Xícara", "Azul com desenho", 35.94, 40},
+		{"Sapato", "Couro", 99.90, 100},
+		{"Tenis", "Confortável", 89, 3},
+	}
+
+	err := temp.ExecuteTemplate(w, "Index", products)
+	if err != nil {
+		return
+	}
 }
